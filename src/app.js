2c48c4cbe8d5ab16c8fac7533a5988c538d94f55
Jeremy Morgan <jeremy@kodekloud.com>
2024-09-23 15:11:38 -0700

    test perms

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   res.send('Hello, Azure Pipelines and GitHub Actions!');
 });
 
-// try this again
+// try this again!!
 
 if (require.main === module) {
   app.listen(port, () => {
